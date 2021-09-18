d03d8d8df97ea85948fd1b9ab0c17524b57ca524
choihanna <hnc24@naver.com>
2021-09-19 01:00:00 +0900

    3weeks

--- Task#2/#2-3.c
+++ Task#2/#2-3.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     float pi = 3.14159265;
 
-    printf("%f\n%.f\n\t\t\t\t%.4f\n\t\t\t\t%.2f\n\t\t\t\t%.f", pi, pi, pi, pi, pi);
+    printf("%.8f\n%.f\n\t\t\t\t%.4f\n\t\t\t\t%.2f\n\t\t\t\t%.f", floor(pi * 100000000) / 100000000, pi, floor(pi * 10000) / 10000, pi, pi);
+
+    return 0;
 }
