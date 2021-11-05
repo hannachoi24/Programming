f5275e04d7903e9f737f0e2052a2412821fb0d69
choihanna <hnc24@naver.com>
2021-11-05 16:07:58 +0900

    9weeks

--- Task#7/7_function.c
+++ Task#7/7_function.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/* 두 개의 정수를 입력 받은 후 사칙연산 (덧셈, 뺄셈, 곱셈, 나눗셈) 을 수행한 후, 결과값을 return 하는 4개 함수를 작성.
+ (아래 main 함수가 올바른 값을 출력하도록 적절하게 함수를 작성) */
+
 int iadd(int a, int b)
 {
     return a + b;
